061264d1b9c07e92a0cb4bf7b2f1c42ae5de3f3b
Theonilah Owali <theonilahtash@gmail.com>
2018-11-22 22:30:44 +0300

    added a routing to display the profile picture

--- app/main/views.py
+++ app/main/views.py
@@ -35,5 +35,17 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
         return render_template('profile/update.html',form=form)
 
-        
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+        return redirect(url_for('main.profile',unmae=unmae))
+
+
 
