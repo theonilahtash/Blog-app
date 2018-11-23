3bac5ebcd57254424dce189aee8e66351731cd3d
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 15:03:53 +0300

    registered blueprint in my initiatilizing file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 # Configurations
 login_manager = LoginManager()
@@ -19,9 +20,7 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
-    # configure UploadSet
-    configure_uploads(app,photos)
-
+   
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
@@ -37,5 +36,8 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+    
+     # configure UploadSet
+    configure_uploads(app,photos)
 
     return app
