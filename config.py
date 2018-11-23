587432440f725104f51165e8d66228c0694d0db1
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 12:18:07 +0300

    this will export the variables to the environment

--- config.py
+++ config.py
@@ -3,6 +3,15 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa1234@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+
+# email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("theonilahtash")
+    MAIL_PASSWORD = os.environ.get("tash@1234")
 
 
 class ProdConfig(Config):
