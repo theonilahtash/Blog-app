40565473f67a5b72f1c23473ee7ba82275053641
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 15:48:44 +0300

    extended my wtf quick forms in the login file

--- config.py
+++ config.py
@@ -2,15 +2,16 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa1234@localhost/blog'
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
 
 
 # email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("theonilahtash@gmail.com")
+    MAIL_USERNAME = os.environ.get("nilah@gmail.com")
     MAIL_PASSWORD = os.environ.get("tash@1234")
 
 
