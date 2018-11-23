42b62915b3858611fab31953a18ef6d04e7bb01f
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 08:20:08 +0300

    added the sqlalchemy database for storing my appplication data

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa1234@localhost/blog'
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:moringa1234@localhost/blog'
+    DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
