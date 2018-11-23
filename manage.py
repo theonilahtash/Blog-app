3bd200a4eacba617cd1f15914ebe934171e0ecb6
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 08:22:12 +0300

    created the app instance

---
+++ manage.py
@@ -0,0 +1,22 @@
+from app import create_app, db
+from flask_script import Manager,Server
+from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Category, Pitch, Comment
+
+
+# Creating app instance
+app = create_app('production')
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+# app = create_app('development')
+
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db, User = User, Category = Category, Pitch = Pitch, Comment = Comment)
+
+if __name__ == '__main__':
+    manager.run()
