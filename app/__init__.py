84b3985aa07890906cd70bd4662b49108bb0d375
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 12:05:19 +0300

    used the configure_uploads function to pass in the app instance and the uploadset instance

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,9 @@ def create_app(config_name):
 
     app = Flask(__name__)
 
+    # configure UploadSet
+    configure_uploads(app,photos)
+
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
