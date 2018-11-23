b2962fe08907bbb7d384eb70567d3b7be01ae40d
Theonilah Owali <theonilahtash@gmail.com>
2018-11-23 15:06:59 +0300

    created a database and made migrations

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from . import auth
 from ..models import User
 from .forms import RegistrationForm, LoginForm
-from .. import db
+from .. import db, photos
 from flask_login import login_user, current_user, logout_user
 from ..email import mail_message
 
--- app/main/views.py
+++ app/main/views.py
@@ -2,9 +2,8 @@ from flask import render_template,redirect,url_for,abort
 from ..models import User,Blog,Comment,Subscriber
 from .forms import UpdateProfile,BlogForm,CommentForm,SubscriberForm
 from . import main
-from ..import db,photos
-from flask_login import login_required,current_
-User
+from ..import db, photos
+from flask_login import login_required,current_user
 from datetime import datetime
 from ..email import mail_message
 
